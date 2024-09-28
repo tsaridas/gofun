33bced7f2d0a0e6d593a8aa8d9e05935fed6679c
Andreas Tsaridas <andreas.tsaridas@gmail.com>
2024-09-28 17:21:21 +0000

    init

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins: true, // Allow all origins
+		// You can customize other options here
+	}))
+	r.Any("/*path", func(c *gin.Context) {
+		message := "Welcome to our API 2 at " + time.Now().Format(time.RFC3339)
+		c.JSON(http.StatusOK, gin.H{"message": message})
+	})
+	r.Run(":3000")
+}
